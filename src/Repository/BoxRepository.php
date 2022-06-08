d076ca8e92a0f3305cee2c4dc29f6fabe9d853af
Alex <alguti@zoho.com>
2022-06-08 19:37:50 +0200

    added find boxes and box name

--- src/Repository/BoxRepository.php
+++ src/Repository/BoxRepository.php
@@ -39,6 +39,26 @@ class BoxRepository extends ServiceEntityRepository
         }
     }
 
+    public function getBoxes(): array
+    {
+        $entityManager = $this->getEntityManager();
+        $query = $entityManager->createQuery(
+            'SELECT box.name
+             FROM App\Entity\Box box'
+        );
+        return $query->getResult();
+    }
+
+    public function getBoxesName(): array
+    {
+        $entityManager = $this->getEntityManager();
+        $query = $entityManager->createQuery(
+            'SELECT box.name
+             FROM App\Entity\Box box'
+        );
+        return $query->getSingleColumnResult();
+    }
+
 //    /**
 //     * @return Box[] Returns an array of Box objects
 //     */
