d1d98fe1dc6873103b4cd824fc91a8138ef7a040
Alex <alguti@zoho.com>
2022-06-08 19:38:49 +0200

    generate recalls

--- src/Controller/rememberController.php
+++ src/Controller/rememberController.php
@@ -6,7 +6,8 @@ use App\Entity\Box;
 use App\Entity\Recall;
 use App\Form\Type\BoxType;
 use App\Form\Type\RecallType;
-
+use App\Repository\BoxRepository;
+use App\Repository\RecallRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,25 +19,36 @@ class RememberController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function showIndex(ManagerRegistry $doctrine, Request $request): Response
+    public function showIndex(ManagerRegistry $doctrine, Request $request, BoxRepository $boxRepository, RecallRepository $recallRepository): Response
     {
         $box = new Box();
         $recall = new Recall();
         $box->setName('new box');
         $entityManager = $doctrine->getManager();
         $form = $this->createForm(Boxtype::class,$box);
-        $form->handleRequest($request);
-        $recallForm = $this->createForm(RecallType::class,$recall);
-        
 
+        $recallForm = $this->createForm(RecallType::class,$recall);
+        $recallForm->handleRequest($request);
+        $boxes = $boxRepository->getBoxes();
 
         if($form->get('new')->isClicked())
         {
             //$entityManager->persist($box);
             //$entityManager->flush();
-            return $this->renderForm('remember/index.html.twig',['form' => $form , 'log' => 'no new boxes for now']);
+            return $this->renderForm('remember/index.html.twig',['form' => $form ,'recallForm' => $recallForm, 'log' => 'no new boxes for now']);
+        }
+        //Need to 
+        if($recallForm->get('recall')->isClicked())
+        {
+            $boxName = $recallForm->get('boxName')->getData();
+            $box = $boxRepository->findOneBy(['name' => $boxName]);
+            $recall->setName('new recall');
+            $recall->setTargetBox($box);
+            //$entityManager->persist($recall);
+            //$entityManager->flush();
+            return $this->renderForm('remember/index.html.twig',['form' => $form ,'recallForm' => $recallForm,'boxes' => $boxes, 'log' => 'you did great']);
         }
 
-        return $this->renderForm('remember/index.html.twig',['form' => $form,'recallForm' => $recallForm]);
+        return $this->renderForm('remember/index.html.twig',['form' => $form,'recallForm' => $recallForm,'boxes' => $boxes]);
     }
 }
